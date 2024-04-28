e71e740faf3cf7b27ef58561c4da72d71472a27f
SilverGM14 <161602519+SilverGM14@users.noreply.github.com>
2024-04-28 16:41:46 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ var imagengenerador = document.getElementById("imagenqr");
 
 formularioQR.addEventListener('submit', function(event) {
     event.preventDefault();
-
     var nombre = document.getElementById("inputnombre").value;
     var apellidos = document.getElementById("inputapellidos").value;
     var juego = document.getElementById("inputjuego").value;
-
     var datos = "Nombre: " + nombre + ", Apellidos: " + apellidos + ", Videojuego Favorito: " + juego;
     var url = "https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=" + encodeURIComponent(datos);
     imagengenerador.src = url;
     alert("Código QR generado");
+});
